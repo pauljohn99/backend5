145c0f2a7648053c2f8f5327bc6724eaaca1abc0
pauljohn99 <paul.j.alapatt123@gmail.com>
2022-08-16 09:36:50 +0530

    finished

--- ArraySorting.java
+++ ArraySorting.java
@@ -1,5 +1,20 @@
 package com.orthofx;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 public class ArraySorting {
+	public static void main(String[] args){
+		Scanner read=new Scanner(System.in);
+		 ArrayList<String> name = new ArrayList<String>();
+		System.out.println("enter names");
+		for(int i=0;i<10;i++) {
+			String in=read.next();
+			name.add(in);
+		}
+		Collections.sort(name);
+		System.out.println(name);
+			
+		}
+		}
 
-}
